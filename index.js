632640f26a56f6615409b726e64510e2d8dc2f02
faizan <faizan.ah862@gmail.com>
2024-06-12 13:31:02 +0300

    delete a todo
    added the delete todo functionality
    
    added a css file

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
 const todoList = []
 const todoInput = document.getElementById("inputTodo")
 const todoBtn = document.getElementById("btnTodo")
-
+let labelCountId = 0
 //add todo
 const addTodo = (event) => {
     event.preventDefault()
     const todo = todoInput.value.trim()
-    if(todo.length){
+    if (todo.length) {
         todoList.push(todo)
         todoInput.value = ""
         displayTodo(todo)
-    }else{
+    } else {
         alert("add a todo item")
     }
-} 
-//delete todo
-const deleteTodo = (value) => {
-
 }
+//delete todo
+const deleteTodo = (value, label) => {
+    const index = todoList.indexOf(value)
+    if (index > -1) {
+        todoList.splice(index, 1);
+    }
+    const allTodos = document.querySelectorAll("li")
+    allTodos.forEach(todo => {
+        const lab = todo.querySelector("label")
+        if (value === lab.innerHTML && label.id === lab.id) {
+            todo.remove()
+        }
+    })
+}
+
 //edit todo
 
 //display todo list
 const displayTodo = (value) => {
     const todoListEl = document.getElementById("todoList")
     let li = document.createElement("li");
-    li.innerHTML = value
+    let label = document.createElement("label");
+    label.setAttribute("id", `label-${++labelCountId}`)
+    label.innerHTML = value
+    var checkbox = document.createElement("INPUT");
+    checkbox.setAttribute("type", "checkbox");
+
+    const btn = document.createElement('button');
+    btn.type = 'button';
+    btn.innerHTML = 'Delete';
+    btn.style.marginLeft = "10px"
+    btn.addEventListener("click", () => deleteTodo(value, label));
+    li.appendChild(checkbox)
+    li.appendChild(label)
+    li.appendChild(btn)
     todoListEl.appendChild(li)
 }
 
