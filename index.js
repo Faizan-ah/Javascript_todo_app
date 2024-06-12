a3eec15123106728c73719fd3e14880594350576
faizan <faizan.ah862@gmail.com>
2024-06-12 16:15:58 +0300

    Persist Todos
    - added persistant storage
    
    - added styles

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const todoInput = document.getElementById("inputTodo")
 const todoBtn = document.getElementById("btnTodo")
+const todoList = []
 let labelCountId = 0
 
 //add todo
@@ -7,14 +8,20 @@ const addTodo = (event) => {
     event.preventDefault()
     const todo = todoInput.value.trim()
     if (todo.length) {
+        todoList.push(todo)
         todoInput.value = ""
         displayTodo(todo)
     } else {
         alert("add a todo item")
     }
 }
+
 //delete todo
 const deleteTodo = (value, label) => {
+    const index = todoList.indexOf(value);
+    if (index > -1) {
+        todoList.splice(index, 1);
+    }
     const allTodos = document.querySelectorAll("li")
     allTodos.forEach(todo => {
         const lab = todo.querySelector("label")
@@ -22,6 +29,11 @@ const deleteTodo = (value, label) => {
             todo.remove()
         }
     })
+    if (todoList.length === 0) {
+        localStorage.removeItem("todoList")
+    } else {
+        localStorage.setItem("todoList", todoList)
+    }
     updateTodoCounter()
 }
 
@@ -29,29 +41,50 @@ const deleteTodo = (value, label) => {
 
 //display todo list
 const displayTodo = (value) => {
+    createList(value)
+    localStorage.setItem("todoList", todoList)
+    updateTodoCounter()
+}
+
+const createList = (value) => {
     const todoListEl = document.getElementById("todoList")
     let li = document.createElement("li");
     let label = document.createElement("label");
     label.setAttribute("id", `label-${++labelCountId}`)
     label.innerHTML = value
-    var checkbox = document.createElement("INPUT");
+    var checkbox = document.createElement("input");
     checkbox.setAttribute("type", "checkbox");
 
-    const btn = document.createElement('button');
-    btn.type = 'button';
-    btn.innerHTML = 'Delete';
-    btn.style.marginLeft = "10px"
-    btn.addEventListener("click", () => deleteTodo(value, label));
+    let btnDiv = document.createElement("btnDiv");
+    btnDiv.setAttribute("class", "btnDiv")
+
+    const delBtn = document.createElement('button');
+    delBtn.setAttribute("class", "delBtn")
+    delBtn.type = 'button';
+    delBtn.innerHTML = 'Delete';
+    delBtn.style.marginLeft = "10px"
+    delBtn.addEventListener("click", () => deleteTodo(value, label));
+
     li.appendChild(checkbox)
     li.appendChild(label)
-    li.appendChild(btn)
+    btnDiv.appendChild(delBtn)
+    li.appendChild(btnDiv)
     todoListEl.appendChild(li)
-    updateTodoCounter()
 }
 
-const updateTodoCounter = () =>{
+const updateTodoCounter = () => {
     const totalTodos = document.getElementById("totalTodos")
     totalTodos.innerHTML = document.querySelectorAll("li").length
 }
+
+//persistant storage
+const persistedTodos = localStorage.getItem("todoList")?.split(",")
+persistedTodos?.forEach(todo => {
+    todoList.push(todo)
+    createList(todo)
+})
+
+updateTodoCounter()
+
 //events
 todoBtn.addEventListener('click', addTodo)
